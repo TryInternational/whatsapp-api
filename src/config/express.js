78bd6178667d779f8ecb9e179b4f3da5633626ea
syedaamish <aamish@try.city>
2023-02-02 16:56:01 +0530

    cros

--- src/config/express.js
+++ src/config/express.js
@@ -6,7 +6,25 @@ const app = express()
 const error = require('../api/middlewares/error')
 const tokenCheck = require('../api/middlewares/tokenCheck')
 const { protectRoutes } = require('./config')
+const cors = require('cors')
 
+// enable cors
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    )
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    next()
+})
+app.options('*', cors())
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
